27ebcd2d10869d092136b67b1783ada7d9195a25
Anthony Smith <anthony@sticksnleaves.com>
2017-03-17 18:26:47 -0400

    First!

---
+++ lib/versionary.ex
@@ -0,0 +1,18 @@
+defmodule Versionary do
+  @moduledoc """
+  Documentation for Versionary.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Versionary.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
