6f86edc4d20c45bdcba070b171d698e79d4eaf16
Anthony Smith <anthony@sticksnleaves.com>
2017-03-18 17:17:47 -0400

    Mix application fix for 1.3

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule Versionary.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
