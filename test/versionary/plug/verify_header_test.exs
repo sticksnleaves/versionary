346064dd939d8969be6aad67d64d1ac4dd2884e7
Anthony Smith <anthony@sticksnleaves.com>
2017-05-06 00:52:05 -0400

    Added support for verifying versions against mime

--- test/versionary/plug/verify_header_test.exs
+++ test/versionary/plug/verify_header_test.exs
@@ -10,6 +10,7 @@ defmodule Versionary.Plug.VerifyHeaderTest do
   @opts1 VerifyHeader.init([versions: [@v1]])
   @opts2 VerifyHeader.init([header: "x-version", versions: [@v1]])
   @opts3 VerifyHeader.init([versions: [@v1, @v2]])
+  @opts4 VerifyHeader.init([accepts: [:v1]])
 
   test "init/1 sets the header option to the value passed in" do
     assert @opts2[:header] == "x-version"
@@ -34,6 +35,15 @@ defmodule Versionary.Plug.VerifyHeaderTest do
       conn(:get, "/")
       |> put_req_header("accept", @v2)
       |> VerifyHeader.call(@opts1)
+
+    assert conn.private[:version_verified] == false
+  end
+
+  test "verification fails if mime is incorrect" do
+    conn =
+      conn(:get, "/")
+      |> put_req_header("accept", @v2)
+      |> VerifyHeader.call(@opts4)
 
     assert conn.private[:version_verified] == false
   end
@@ -70,6 +80,15 @@ defmodule Versionary.Plug.VerifyHeaderTest do
       conn(:get, "/")
       |> put_req_header("accept", @v1)
       |> VerifyHeader.call(@opts3)
+
+      assert conn.private[:version_verified] == true
+  end
+
+  test "verification succeeds if at least one mime matches" do
+    conn =
+      conn(:get, "/")
+      |> put_req_header("accept", @v1)
+      |> VerifyHeader.call(@opts4)
 
       assert conn.private[:version_verified] == true
   end
