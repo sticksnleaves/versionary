978a26f7f3029c668117f8d624e894afd272a37c
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-10-17 09:19:26 -0400

    Only compile PhoenixErrorHandler if Phoenix is loaded

--- lib/versionary/plug/phoenix_error_handler.ex
+++ lib/versionary/plug/phoenix_error_handler.ex
@@ -1,28 +1,17 @@
-defmodule Versionary.Plug.PhoenixErrorHandler do
-  @moduledoc """
-  An error handler for usage with Phoenix.
+if Code.ensure_loaded?(Phoenix) do
+  defmodule Versionary.Plug.PhoenixErrorHandler do
+    @moduledoc """
+    An error handler for usage with Phoenix.
 
-  When called this handler raise a `Phoenix.NotAcceptableError` triggering the
-  `406.json` error view.
-  """
+    When called this handler raise a `Phoenix.NotAcceptableError` triggering the
+    `406.json` error view.
+    """
 
-  @behaviour Versionary.Plug.Handler
+    @behaviour Versionary.Plug.Handler
 
-  def call(_conn) do
-    verify_phoenix_dep()
-
-    raise Phoenix.NotAcceptableError,
-      message: "no supported media type in accept header"
-  end
-
-  # private
-
-  defp verify_phoenix_dep do
-    unless Code.ensure_loaded?(Phoenix) do
-      raise """
-      You tried to use Versionary.Plug.PhoenixErrorHandler, but the Phoenix
-      module is not loaded. Please add phoenix to your dependencies.
-      """
+    def call(_conn) do
+      raise Phoenix.NotAcceptableError,
+        message: "no supported media type in accept header"
     end
   end
 end
