e151439a3b83b6dd920cc33fcb8f645ddf7d49e0
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-10-17 07:07:07 -0400

    Clean up mix.exs

--- mix.exs
+++ mix.exs
@@ -21,23 +21,10 @@ defmodule Versionary.Mixfile do
      test_coverage: [tool: ExCoveralls]]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:plug, "~> 1.3"},
